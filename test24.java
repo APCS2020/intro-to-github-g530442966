66d7f03203cba3b3ff2f774d5e65816fde3e60cd
郭建国 Jim <48339906+g530442966@users.noreply.github.com>
2019-05-17 10:34:42 +0800

    Add files via upload

---
+++ test24.java
@@ -0,0 +1,11 @@
+public class test24{
+    public static void printLine(int num){
+        for(int i = 0; i < num; i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        printLine(12);
+    }
+}
